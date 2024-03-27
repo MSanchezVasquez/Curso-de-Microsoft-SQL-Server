445f7c84af6f2b4aac7ffa74c12b631eaeb09d79
MSanchezVasquez <moisesup.25@gmail.com>
2024-03-27 11:48:36 -0500

    importar base de datos

---
+++ 28. RESTAURAR UNA BASE DE DATOS (Adventureworks 2022).sql
@@ -0,0 +1,7 @@
+-- Ingresar a adventuresworks base de datos en google.
+
+--Dale click a la carpeta databases, luego click en restore database, dale a la opcion device y luego a add, busca el archivo descargado en la pagina.
+
+use AdventureWorks2022;
+
+select * from HumanResources.Employee;
